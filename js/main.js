0905fdda22e1b3ad21acdbf47909f29782b2908a
beatarynkevic <beatainn@gmail.com>
2020-11-15 13:35:40 +0200

    Veiksmai

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-//Kintamuju inicijavimas
+//1.Kintamuju inicijavimas
+
 //Data types
 //Number
 
@@ -21,5 +22,37 @@ const lastName = 'Rynkevic';
 const math = [8,5,2,10,9];
 console.log(math);
 
-const names = ['Beata','Edita','Karolina','Demante','Ana'];
+const names = ['Beata','Edita','Karolina','Demante','Ana']; //0,1,2,3,4
 console.log(names);
+
+//2.Veikmai su kintamaisiais
+
+//Susumuoti skaiciaus tipo kintamuosius
+const sum = age + luckyNumber + income;
+console.log(sum);
+
+//Sujungti teksto tipo kinstamuosius, kad tarp ju butu tarpas
+
+const about = name + ' ' + lastName + ' ' + city;
+console.log(about);
+
+//Apskaiciuoti sarasu verte, salyga: 1-2+3-4+5
+
+let numbers = 0;
+numbers = numbers + math[0];
+numbers = numbers - math[1];
+numbers = numbers + math[2];
+numbers = numbers - math[3];
+numbers = numbers + math[4];
+console.log(numbers);
+
+/*Sujungti sarasu vertes, kuriu tipas yra tekstas,
+salyga: nuo gali iki pradzios, o tarp ju kablelis ir tarpas*/
+
+let friends = '';
+friends = friends + names[4];
+friends = friends + ', ' + names[3];
+friends = friends + ', ' + names[2];
+friends = friends + ', ' + names[1];
+friends = friends + ', ' + names[0];
+console.log(friends);
