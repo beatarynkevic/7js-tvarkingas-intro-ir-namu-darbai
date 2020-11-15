e58f7f6f00806163b482d0262bc00770ab3eb9bf
beatarynkevic <beatainn@gmail.com>
2020-11-15 13:21:18 +0200

    Kintamuju inicijavimas

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Kintamuju inicijavimas
+//Data types
+//Number
+
+const age = 25;
+console.log(age);
+
+const luckyNumber = 13;
+const income = 100;
+
+//String
+
+const city = 'Vilnius';
+console.log(city);
+
+const name = 'Beata';
+const lastName = 'Rynkevic';
+
+//Array
+
+const math = [8,5,2,10,9];
+console.log(math);
+
+const names = ['Beata','Edita','Karolina','Demante','Ana'];
+console.log(names);
