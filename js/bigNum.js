a6e7f316f99950b31fe60a10fa786ebbcc8fc0a6
beatarynkevic <beatainn@gmail.com>
2020-11-30 12:19:49 +0200

    bigNum renew

--- js/bigNum.js
+++ js/bigNum.js
@@ -1,22 +1,24 @@
-function bigNum (list) {
+function bigNum(list) {
     //input validation
     if (typeof list !== 'object') {
         return 'Netinkamas tipas'
     }
 
     const size = list.length;
+
     if (size === 0) {
         return 'Sarasas yra tuscias.'
     }
+
     //logic
-    let biggestNumber = list[0];
+    let biggestNumber = -Infinity;
 
     for (let i = 1; i < size; i++) {
-        const num = list[0];
-        
+        const num = list[i];
+
         //saraso elemento validacija
         if (typeof num !== 'number') {
-            continue; //keyword, kuris naudojamas cikle,padaro : nueina i nauja cikla, praleides apacioje esancia logika
+            continue;
         }
 
         //logika
@@ -25,20 +27,48 @@ function bigNum (list) {
         }
     }
 
-    return 0;
-    //return
+    //post logic validation
+    if (biggestNumber === -Infinity) {
+        return 'Sarase nerastas nei vienas normalus skaicius.';
+    }
+
+    // return
+    return biggestNumber;
 }
 
-console.log(bigNum([1]), '->', 1);
-console.log(bigNum([1, 2, 3 ]), '->', 3);
+// demo funkcija, skirta testams sudaryti
+function sum(a, b) {
+    return a + b;
+}
 
-console.log(bigNum([]));
 console.log(bigNum('pomidoras'));
 console.log(bigNum(585));
 console.log(bigNum(true));
 console.log(bigNum(false));
+console.log(bigNum([]));
+console.log(bigNum([-Infinity, NaN, true, false, [], [8], [5, -9], 'ads', undefined]));
+console.log(bigNum(sum));
+console.log(bigNum([sum]));
+console.log(bigNum({}));
+console.log(bigNum([{}, {}, {}]));
 
-console.log(bigNum([-5, 78, 14, 0, 18], '->', 78));
-console.log(bigNum([69, 69, 69, 69, 66 ]), '->', 69);
+console.log(bigNum([1]), '->', 1);
+console.log(bigNum([77]), '->', 77);
+console.log(bigNum([1, 2, 3]), '->', 3);
+console.log(bigNum([1, 2, 3, 2, 1]), '->', 3);
+console.log(bigNum([-5, 78, 14, 0, 18]), '->', 78);
+console.log(bigNum([69, 69, 69, 69, 66]), '->', 69);
 console.log(bigNum([-1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
+console.log(bigNum([-1, -2, -3, -4, 79, -6, -7, -8]), '->', 79);
 
+console.log(bigNum([1, 2, 3, 2, 1, 'labas']), '->', 3);
+console.log(bigNum([1, 2, 3, 'labas', 2, 1]), '->', 3);
+console.log(bigNum(['labas', 1, 2, 3, 2, 1]), '->', 3);
+
+console.log(bigNum(['labas', 'labas', 'labas', 'labas', -99999999999999]), '->', -99999999999999);
+console.log(bigNum(['labas', 'labas', -99999999999999, 'labas', 'labas']), '->', -99999999999999);
+console.log(bigNum([-99999999999999, 'labas', 'labas', 'labas', 'labas']), '->', -99999999999999);
+
+console.log(bigNum([1, 2, 3, 2, 1, NaN]), '->', 3);
+console.log(bigNum([1, 2, NaN, 3, 2, 1]), '->', 3);
+console.log(bigNum([NaN, 1, 2, 3, 2, 1]), '->', 3);
